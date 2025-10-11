566033bd79490993fcc66c70c22a0978f5d7f8fa
sofkp <sofia.ku@utec.edu.pe>
2025-10-11 17:15:56 -0500

    funcion para first en producciones

--- parser.py
+++ parser.py
@@ -11,7 +11,19 @@ class item:
         self.left = left
         self.right = tuple(right)
         self.dot = dot
-        self.la = lookahead
+        self.la = lookahead #token terminal A -> .a, $
+    
+    def __eq__(self, other):
+        return (self.left, self.right, self.dot, self.la) == (other.left, other.right, other.dot, other.la)
+    
+    def __hash__(self):
+        return hash((self.left, self.right, self.dot, self.la))
+    
+    def __repr__(self):
+        right = list(self.right)
+        right.insert(self.dot,".")
+        right_s = " ".join(right) if right else "."
+        return f"[{self.left} -> {right_s}, {self.la}]" 
 
 
 def gramatica(filename):
@@ -97,3 +109,21 @@ def first_compute(regla, nterminal, terminal):
                     c = True
 
     return first                
+
+def first_prod(prod, first, nterminal): #para calulcar los first en produccion expandida
+    res = set()
+    if not prod:
+        res.add("''")
+        return res
+    
+    for s in prod:
+        if s in nterminal:
+            agregar = [] #los terminales de la variable 
+            for i in first[s]:
+                if i != "''" : agregar.append(i) #agregar todos menos epsilon
+            res.update(agregar)
+        else:
+            res.add(s)
+            return res
+    res.add("''")
+    return res
